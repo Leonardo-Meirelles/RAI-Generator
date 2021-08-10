7e2f7c8dd7ae5e3215d1a4a17704ca548c104b2a
Leonardo-Meirelles <leonardomeirelles@terra.com.br>
2021-08-10 07:46:29 -0300

    organizei a pasta server

--- app/server.js
+++ app/server.js
@@ -2,13 +2,12 @@ import Express from 'express';
 import Path from 'path';
 import Routes from './routes/routes.js'
 
-
-
 const app = Express();
 const __dirname = Path.resolve();
 
 app.use(Express.json())
 app.use(Express.urlencoded({extended:true}))
+
 //set view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -17,9 +16,6 @@ app.use(Express.static (Path.join(__dirname + '/app','/public')));
 app.set('views', Path.join(__dirname + '/app', 'views'));
 
 Routes(app);
-
-//     //req.body.telefone - forma de buscar todos os name dentro do html(telefone nesse caso seria de uma tag html)
-
 
 const port = 3000;
 app.listen(port, () => {
